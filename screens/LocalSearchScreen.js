4289e757a1bbdefe91d1be7222b987ee5ff650b5
BenCloughessy <110562848+BenCloughessy@users.noreply.github.com>
2022-11-17 17:47:50 -0500

    implement placeFinder

--- screens/LocalSearchScreen.js
+++ screens/LocalSearchScreen.js
@@ -4,51 +4,66 @@ import { Button, View, Text, Platform } from "react-native";
 import { StyleSheet } from "react-native";
 import * as Location from 'expo-location';
 
-
-
-
 const LocalSearchScreen = () => {
     const [location, setLocation] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
     const [errorMsg, setErrorMsg] = useState(null)
+    const [results, setResults] = useState([])
+    let text
 
-    const requestPermission = async() => {
-        setIsLoading(true)
-        let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== 'granted') {
-          setErrorMsg('Permission to access location was denied');
-          return;
-        }
+    const getLocation = async() => {
+      setIsLoading(true)
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
         
-        let location = await Location.getCurrentPositionAsync({});
-        setLocation(location);
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
     }
-    
-    let text = ''
+
+    useEffect(() => {
+      if (location) {
+        shelterSearch()
+      }
+      
+    }, [location])
+
+
+    const shelterSearch = async() => {
+      const lat = location.coords.latitude
+      const lng = location.coords.longitude
+  
+      let placeFinder = new PlaceFinder('aWYBPDg8q4jsUHu3EViMzBg3kJi91gaV');
+      let results = await placeFinder.getNearbyPlaces(lat, lng)
+      setResults(results);
+      
+      return results
+    }
+
       if (isLoading) {
         text = 'Waiting..';
       } 
-      
       if (errorMsg) {
         text = errorMsg;
-      } 
-      
+      }
       if (location) {
-        text = `Lat: ${location.coords.latitude} Long: ${location.coords.longitude}`;
+        text = location.coords.latitude
+      }
+      if (results) {
+        console.log('results', results)
       }
 
     return (
         <View style={styles.container}>
             <View style={{marginTop: 10, padding: 10, borderRadius: 10, width: '40%'}}>
-                <Button title="Get Location" color='#60b593' onPress={requestPermission} />
+                <Button title="Get Location" color='#60b593' onPress={getLocation} />
             </View>
             <Text>{text}</Text>
         </View>
-
-        
-        
     );
- 
 }
 
 const styles = StyleSheet.create({
